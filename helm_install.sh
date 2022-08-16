42e6d6203ab5a77d18da4c6a0bd106008d649f5b
lalifeier <lalifeier@gmail.com>
2022-08-16 23:22:44 +0800

    init

--- helm_install.sh
+++ helm_install.sh
@@ -2,7 +2,12 @@
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-helm repo add stable https://charts.helm.sh/stable
+# helm repo remove stable
+# helm repo add stable https://charts.helm.sh/stable
+helm repo add stable http://mirror.azure.cn/kubernetes/charts
+helm repo add aliyun https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+helm repo update
 
-helm repo remove stable
-helm repo add stable https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+# helm repo list
+# helm search repo stable
+# helm repo remove aliyun
