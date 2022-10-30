c8b1a955442a527b15de8cfaf4dc204c57905974
lalifeier <lalifeier@gmail.com>
2022-10-30 23:52:24 +0800

    init

--- ci/k3d/intsall.sh
+++ ci/k3d/intsall.sh
@@ -17,5 +17,16 @@ k3d create -n k3s-local -i rancher/k3s:v1.24.4-k3s1  -v $(pwd)/airgap/v1.24.4/:/
 
 kubectl get nodes
 
+# kubecm
+# brew install kubecm
+
+# curl -Lo kubecm.tar.gz https://github.com/sunny0826/kubecm/releases/download/v${VERSION}/kubecm_${VERSION}_Linux_x86_64.tar.gz
+# tar -zxvf kubecm.tar.gz kubecm
+# cd kubecm
+# sudo mv kubecm /usr/local/bin/
+
+# kubecm add -f $(k3d get-kubeconfig --name='k3s-local') -n k3s -c
+# kubecm s
+
 
 # k3d cluster delete mycluster
