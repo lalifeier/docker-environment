9309e6338c4ad5b8d1dc8d6c391905c3733821e5
lalifeier <lalifeier@gmail.com>
2022-11-14 03:17:08 +0000

    init

---
+++ queue/pulsar/standalone/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# sudo docker cp pulsar:/pulsar/conf ./pulsar
+
+# https://pulsar.apache.org/docs/administration-pulsar-manager/
+# http://localhost:9527
+
+CSRF_TOKEN=$(curl http://localhost:7750/pulsar-manager/csrf-token)
+curl \
+   -H 'X-XSRF-TOKEN: $CSRF_TOKEN' \
+   -H 'Cookie: XSRF-TOKEN=$CSRF_TOKEN;' \
+   -H "Content-Type: application/json" \
+   -X PUT http://localhost:7750/pulsar-manager/users/superuser \
+   -d '{"name": "admin", "password": "apachepulsar", "description": "test", "email": "username@test.org"}'
