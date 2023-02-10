35354bc6050f99143e720220f5eeda3d608713e3
lalifeier <lalifeier@gmail.com>
2023-02-10 20:56:21 +0800

    init

---
+++ clash/init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Country.mmdb
+wget https://github.com/Dreamacro/maxmind-geoip/releases/download/20230112/Country.mmdb
+# wget https://cdn.jsdelivr.net/gh/baixf-xyz/baixf-xyz@latest/Country.mmdb
+# wget https://www.sub-speeder.com/client-download/Country.mmdb
+
+
+# config.yaml
+# wget -O /etc/clash/config.yaml [你的订阅链接]
+
+# port: 7890
+# socks-port: 7891
+# external-controller: :9090
+# external-ui: /ui
+
+# ui
+# clash-dashboard
+# git clone -b gh-pages --depth 1 https://github.com/Dreamacro/clash-dashboard ui
+# yacd-dashboard
+# git clone -b gh-pages --depth 1 https://github.com/haishanh/yacd ui
