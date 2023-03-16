a864fc2091b6ecff4ce5bd7381f27ee005f9590a
lalifeier <lalifeier@gmail.com>
2023-03-16 15:57:42 +0000

    init

---
+++ cdn/apache-traffic-server/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#原版（英文界面）：
+wget https://raw.githubusercontent.com/Har-Kuun/OneClickCDN/master/OneClickCDN.sh && sudo bash OneClickCDN.sh
+
+#中文版：
+wget https://raw.githubusercontent.com/Har-Kuun/OneClickCDN/master/translation/translated_scripts/OneClickCDN_zh-CN.sh && sudo bash OneClickCDN_zh-CN.sh
---
+++ cdn/fikker/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+# https://www.fikker.com/download.htm
